94dc83e25d2b26f137cc20fb1a85e34a66334293
Siskin <siskinvladimir98@gmail.com>
2020-05-14 15:12:38 +0200

    littleChange1

--- src/components/Headers/MassagePageHeader.js
+++ src/components/Headers/MassagePageHeader.js
@@ -48,13 +48,15 @@ function MassagePageHeader() {
                                 <div>
                                     <h2> Lokacija: </h2>
                                     <h3> Vojvodjanska 32</h3>
+                                    <h3> Majke Jevrosime 12</h3>
 
                                 </div>
                             </Col>
                             <Col className="ml-auto mr-auto text-center " md="4">
                                 <div>
                                     <h2> Kontakt: </h2>
-                                    <h3>  +38163111111</h3>
+                                    <h3> +381631894231</h3>
+                                    <h3> +381621234567</h3>
 
                                 </div>
                             </Col>
